4958f767c5a3756c5f100bee3dd39ce6c0d2ddb7
sbonacho <sbonacho@gmail.com>
2015-12-31 11:04:09 +0100

    -Clean not necesary dependencies -Clean config and locale

--- index.js
+++ index.js
@@ -2,12 +2,10 @@
 var command = require("./lib/slife-command");
 
 var onFulfilled = function () {
-    console.log(" onFulfilled------------------------------------------------------")
     process.exit(0);
 }
 
 var onRejected = function (err) {
-    console.log(" onRejected------------------------------------------------------")
     process.exit(-1);
 }
 
