ead6c980e67f11f98d7e32a282623ca53a2afc83
xe55448 <santiago.garcia-bonacho.contractor@bbva.com>
2016-01-11 11:00:44 +0100

    - Pisco sour refactorization - Change all names and create the object for non gulp execution. - NOT WORKING By de moment

--- shots/add/recipe/shot.js
+++ shots/add/recipe/shot.js
@@ -1,12 +1,18 @@
 'use strict';
 
-var Shot = require('../../../lib/shot'),
-    logger = require('../../../lib/slife-logger');
+var piscosour = require('piscosour'),
+    Shot = piscosour.Sour;
+
 
 module.exports = new Shot({
     name : "ejemplo",
 
     pre : function(){
         logger.info("pre---------->");
+    },
+
+    run : function(){
+
     }
+
 });
