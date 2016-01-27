9fd440f1b5d44853f254d086f77357c93f427e9b
sbonacho <sbonacho@gmail.com>
2016-01-27 15:21:47 +0100

    - each shot return to root dir - some logger more

--- shots/scaffolding/recipe/shot.js
+++ shots/scaffolding/recipe/shot.js
@@ -17,24 +17,13 @@ var createDir = function(dir){
 var shot = new Shot({
     description : "Create a piscosour recipe from a scaffold template",
 
-    config : function(resolve){
-        logger.info("#magenta","config","All configurations labors");
-        resolve();
-    },
-
     run : function(resolve, reject){
         logger.info("#magenta","run","Create new recipe from template");
         createDir(path.join(pwd,params.recipeName));
         process.chdir(path.join(pwd,params.recipeName));
 
         shot.execute("yo",params.addPrompts(["pisco-recipe"], shot.runner.params.prompts),resolve, reject);
-    },
-
-    prove : function(resolve){
-        logger.info("#magenta","prove","Prove that the run execution was ok");
-        resolve();
     }
-
 });
 
 module.exports = shot;
