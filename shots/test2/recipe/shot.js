a938e033db9e0fde8e6ae774e5969517ecf6119f
sbonacho <sbonacho@gmail.com>
2016-02-05 09:47:19 +0100

    - Generation of junit xml at the end of the execution - Normal inyected in params - Now is not necesary to call resolve on every method of a shot. - New straw test for pisco testing

---
+++ shots/test2/recipe/shot.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var piscosour = require('../../..'),
+    Shot = piscosour.Shot;
+
+var shot = new Shot({
+    description : "TEST 2 Brief description of shot",
+
+    check : function(resolve){
+        shot.logger.info("#magenta","check","Check all pre-requisites for the execution");
+    },
+
+    config : function(resolve){
+        shot.logger.info("#magenta","config","Preparing params for main execution");
+    },
+
+    run : function(resolve, reject){
+        shot.logger.info("#magenta","run","Run main execution");
+        reject({keep:true, error: "TEST ERROR"});
+    },
+
+    prove : function(resolve){
+        shot.logger.info("#magenta","prove","Prove that the run execution was ok");
+    },
+
+    notify : function(resolve){
+        shot.logger.info("#magenta","notify","Recollect all execution information and notify");
+    }
+
+});
+
+module.exports = shot;
