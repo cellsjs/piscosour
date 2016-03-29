98dbec0453c2b5ec1f4376eaf1db32ac89c60d0a
xe55448 <santiago.garcia-bonacho.contractor@bbva.com>
2016-03-29 15:58:43 +0200

    chore(lib/config.js, lib/shot.js): more log messages and clean code feat(plugins/cookbook): new plugin for recipes utilities feat(): Refactor of shots and straws in order to use the new plugin.

--- shots/extract-md/recipe/shot.js
+++ shots/extract-md/recipe/shot.js
@@ -3,24 +3,9 @@
 module.exports = {
     description : "Generate info.md files from comments in shots",
 
-    check : function(){
-        this.logger.info("#magenta","check","Check all pre-requisites for the execution");
-    },
-
-    config : function(){
-        this.logger.info("#magenta","config","Preparing params for main execution");
-    },
-
     run : function(){
         this.logger.info("#magenta","run","Run main execution");
-    },
 
-    prove : function(){
-        this.logger.info("#magenta","prove","Prove that the run execution was ok");
-    },
-
-    notify : function(){
-        this.logger.info("#magenta","notify","Recollect all execution information and notify");
     }
 
 };
