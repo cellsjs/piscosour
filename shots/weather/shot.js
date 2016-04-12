306420cab26fbef8fa73c0ed7324b30b0e956172
sbonacho <sbonacho@gmail.com>
2016-04-12 17:35:50 +0200

    chore(): fix spelling

--- shots/weather/shot.js
+++ shots/weather/shot.js
@@ -2,7 +2,7 @@
 
 module.exports = {
     run : function(resolve, reject){
-        this.logger.info("#magenta","run","The wheather in ","#green",this.params.yourCity,"is:");
+        this.logger.info("#magenta","run","Weather in ","#green",this.params.yourCity);
         this.sh("curl http://wttr.in/"+this.params.yourCity, null, true);
     }
 };
