2c900e1386bc936ef92647327ca96f4111b0f364
xe55448 <santiago.garcia-bonacho.contractor@bbva.com>
2016-04-14 13:35:19 +0200

    feat(): show weather sending user agent

--- shots/weather/shot.js
+++ shots/weather/shot.js
@@ -2,7 +2,7 @@
 
 module.exports = {
     run : function(resolve, reject){
-        this.logger.info("#magenta","run","Weather in ","#green",this.params.yourCity);
-        this.sh("curl http://wttr.in/"+this.params.yourCity, null, true);
+        this.logger.info("#magenta","run","Weather in ","#green",this.params.city);
+        this.sh("curl -s -A curl http://wttr.in/"+this.params.city, null, true);
     }
 };
