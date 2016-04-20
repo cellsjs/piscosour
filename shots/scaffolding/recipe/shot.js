995273b15429261c9d757cda71c78bcfda063fcf
xe55448 <santiago.garcia-bonacho.contractor@bbva.com>
2016-04-20 18:21:58 +0200

    feat(*): Check generator-pisco-recipe version on pisco create command.

--- shots/scaffolding/recipe/shot.js
+++ shots/scaffolding/recipe/shot.js
@@ -12,7 +12,6 @@ module.exports = {
         this.logger.info("#magenta","run","Create new recipe from template");
         this.fsCreateDir(this.params.recipeName);
         process.chdir(this.params.recipeName);
-        process.env.NODE_PATH=process.env.NODE_PATH+path.delimiter+path.join(this.config.getDir('module'),"node_modules");
         return this.execute("yo",this.promptArgs(["pisco-recipe"])).then(resolve,reject);
     }
 };
