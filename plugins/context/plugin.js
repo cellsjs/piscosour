7370a5b8a3c4d1d0bd75b13f06b694fb61e9d094
sbonacho <sbonacho@gmail.com>
2016-05-19 11:31:56 +0200

    feat(plugin): disable context checking with command option --b-disableContextCheck

--- plugins/context/plugin.js
+++ plugins/context/plugin.js
@@ -8,10 +8,10 @@ module.exports = {
   check: function() {
     if (!this.params.contextFree) {
       let ami = this.ctxWhoami();
-      if (this._repoType && ami.indexOf(this._repoType) < 0) {
+      if (!this.params.disableContextCheck && this._repoType && ami.indexOf(this._repoType) < 0) {
         throw {error: 'This is not the root of a ' + this._repoType};
       }
-      this.logger.info('This shot is in the root of a ' + ami, '#green', 'OK');
+      this.logger.info('Context checked: ' + ami, '#green', 'OK');
     }
   },
 
