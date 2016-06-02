9449cc584c07c6874c133a72c761b40164ce3528
sbonacho <sbonacho@gmail.com>
2016-06-02 09:36:01 +0200

    feat(*): Log every stage in magenta.

--- shots/test/shot.js
+++ shots/test/shot.js
@@ -3,25 +3,15 @@
 module.exports = {
 
   config: function(resolve) {
-    this.logger.info('#magenta', 'config', 'Preparing params for main execution');
     this.logger.info('this.params.inflying:', this.params.inflying);
     this.logger.info('this.params.otracosa:', this.params.otracosa);
   },
-
   run: function(resolve, reject) {
-    this.logger.info('#magenta', 'run', 'Run main execution');
     this.logger.info('execution: ', this.params.execution);
     this.testPluginAddon(' - test plugin addon!');
   },
-
-  prove: function(resolve, reject) {
-    this.logger.info('#magenta', 'prove', 'Prove that the run execution was ok');
-  },
-
-  notify: function(resolve) {
-    this.logger.info('#magenta', 'notify', 'Recollect all execution information and notify');
-  },
-
+  prove: function(resolve, reject) {},
+  notify: function(resolve) {},
   emit: function() {
     return {
       localmovida: 'Un valor dado'
--- shots/test2/recipe/shot.js
+++ shots/test2/recipe/shot.js
@@ -2,21 +2,12 @@
 
 module.exports = {
 
-  check: function(resolve) {
-    this.logger.info('#magenta', 'check', 'Check all pre-requisites for the execution');
-  },
-
   config: function(resolve, reject) {
-    this.logger.info('#magenta', 'config', 'this.params.saludo: ', this.params.saludo);
+    this.logger.info('this.params.saludo: ', this.params.saludo);
   },
 
   run: function(resolve, reject) {
-    this.logger.info('#magenta', 'run', 'Run main execution');
     reject({keep: true, error: 'TEST ERROR'});
-  },
-
-  prove: function(resolve) {
-    this.logger.info('#magenta', 'prove', 'Prove that the run execution was ok');
   },
 
   emit: function() {
