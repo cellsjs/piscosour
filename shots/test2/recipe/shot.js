e8ba292e0ec508c0885f5d8976862f25f0b08ebd
sbonacho <sbonacho@gmail.com>
2016-06-15 17:48:16 +0200

    test(): test onError execution in step test

--- shots/test2/recipe/shot.js
+++ shots/test2/recipe/shot.js
@@ -6,15 +6,16 @@ module.exports = {
     this.logger.info('this.params.saludo: ', this.params.saludo);
     this.sh('ls -las', null, true);
   },
-
   run: function(resolve, reject) {
     reject({keep: true, error: 'TEST ERROR'});
   },
-
   emit: function() {
     return {
       flying: this.params.flying
     };
+  },
+  onError(stage, err) {
+    this.logger.info('#red', 'onError is called!!!', 'stage:', '#green', stage, ' - error:', err);
   }
 
 };
