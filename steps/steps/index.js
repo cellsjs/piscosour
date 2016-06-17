5fb766a7249a0b996d65e10923f7a547b06c3a01
jesus.diaz <jesus.diaz.contractor@bbva.com>
2016-06-17 09:46:10 +0200

    feat(*): adapt step creation to new structure

--- steps/steps/index.js
+++ steps/steps/index.js
@@ -7,42 +7,46 @@ module.exports = {
 
   check: function(go, stop) {
     this.logger.info('#magenta', 'check', 'Check if this is a piscosour recipe');
-    var dest = path.join('steps', this.params.stepName, this.params.context);
+    var dest = path.join('steps', this.params.stepName);
     if (this.fsExists(dest)) {
       stop('Step "' + this.params.stepName + '" already exists for context: "' + this.params.context + '" in this recipe, edit it to change!');
     }
   },
 
-  config: function(go, stop) {
-    this.logger.info('#magenta', 'config', 'Configure recipe for the new step in', this.piscoFile);
-    var configLocal = this.fsReadConfig(this.piscoFile);
-
-    if (!configLocal.contexts) {
-      configLocal.contexts = [];
-    }
-
-    if (configLocal.contexts.indexOf(this.params.context) < 0) {
-      configLocal.contexts.push(this.params.context);
-    }
-    fs.writeFileSync(this.piscoFile, JSON.stringify(configLocal, null, 2));
-  },
-
   run: function(resolve, reject) {
     this.logger.info('#magenta', 'run', 'Creating new step for this recipe');
 
-    var dest = path.join('steps', this.params.stepName, this.params.context);
+    var dest = path.join('steps', this.params.stepName);
     var origin = path.join(this.piscoConfig.getDir('piscosour'), 'templates', '_step');
 
     this.fsCreateDir('steps');
     this.fsCreateDir(path.join('steps', this.params.stepName));
     this.fsCreateDir(dest);
 
-    return this.fsCopyDirFiltered(origin, dest).then(resolve, reject);
+    const _writeConfig = () => {
+      try {
+        const configFile = path.join(dest, 'config.json');
+
+        let config = this.fsReadConfig(configFile);
+        config.name = this.params.stepName;
+        config.contexts.push(this.params.context);
+
+        fs.writeFileSync(configFile, JSON.stringify(config, null, 2));
+
+        return Promise.resolve();
+      } catch (err) {
+        return Promise.reject(err);
+      }
+    };
+
+    return this.fsCopyDirFiltered(origin, dest)
+      .then(() => _writeConfig())
+      .then(resolve, reject);
   },
 
   prove: function(resolve, reject) {
     this.logger.info('#magenta', 'prove', 'Prove if the step is propelly executed');
-    var dest = path.join('steps', this.params.stepName, this.params.context);
+    var dest = path.join('steps', this.params.stepName);
     var result = this.sh('node bin/pisco.js ' + this.params.context + '::' + this.params.stepName, reject, true);
     if (result.status !== 0) {
       this.logger.error('#red', 'Error: step not propelly created!', 'cleaning files!');
