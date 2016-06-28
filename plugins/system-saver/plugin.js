04c6aeab82430c81115e0c45be0b4df603948de3
sbonacho <sbonacho@gmail.com>
2016-06-28 12:44:47 +0200

    chore(): TODO comment

--- plugins/system-saver/plugin.js
+++ plugins/system-saver/plugin.js
@@ -7,6 +7,7 @@ const fs = require('fs');
 module.exports = {
   description: 'System requirements saver',
 
+  //TODO: Mejorar esto 1) guardar el resultado en una variable global en lugar de escribir en disco 2) detectar (this.isLast) el último shot del straw y escribir entonces
   'core-check': function() {
     const fileName = 'requirements.json';
     if (this.params.requirements && this.params.saveRequirements) {
