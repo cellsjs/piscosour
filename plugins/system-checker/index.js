88e08ea75150ca09dd898736bd54b2e076bfb5b6
sbonacho <sbonacho@gmail.com>
2017-03-15 18:43:54 +0100

    feat(): new library for requirements and launcher

--- plugins/system-checker/index.js
+++ plugins/system-checker/index.js
@@ -1,113 +1,17 @@
 'use strict';
 
-// Plugins: ['launcher', 'piscosour', 'fsutils']
+// Plugins: ['launcher']
 
-const semver = require('semver');
-const os = require('os');
-const path = require('path');
-const fs = require('fs');
-const cache = {};
+const requirements = require('../../lib/utils/requirements');
 
 module.exports = {
   description: 'System requirements checker',
 
   'core-check': function() {
 
-    const _getVersion = (cmd, result, options) => {
-      let regexp;
-      if (options.listedIn) {
-        const father = this.params.requirements[options.listedIn];
-        regexp = `${options.key ? options.key : cmd}${options.regexp !== undefined ? options.regexp : father.regexp}`;
-      } else {
-        regexp = options.regexp;
-      }
-      let buffer = !result.stdout || result.stdout.toString().length === 0 ? result.stderr : result.stdout;
-      let version = buffer.toString().replace(os.EOL, '');
-      if (regexp) {
-        const match = buffer.toString().match(regexp);
-        if (match && match.length > 1) {
-          version = match[1].replace(os.EOL, '');
-        } else {
-          version = false;
-        }
-      }
-      return version;
-    };
-    const _cachedExec = (command, noCache) => {
-      let result = noCache ? false : cache[command];
-      if (!result) {
-        result = new Promise((ok, ko) => {
-          this.logger.info('Waiting for', '#green', command, '...');
-          cache[command] = this.sh(command);
-          ok(cache[command]);
-        });
-        cache[command] = result;
-      } else if (!result.then) {
-        result = Promise.resolve(result);
-      }
-      return result;
-    };
-    const _sh = (cmd, options, noCache) => {
-      if (options.listedIn) {
-        const father = this.params.requirements[options.listedIn];
-        if (father && father.list) {
-          this.logger.trace('Getting list for', cmd);
-          return _cachedExec(father.list, noCache);
-        } else {
-          return Promise.reject({error: `There is no definition for listing in ${options.listedIn}`});
-        }
-      } else {
-        let option = options.option ? options.option : '-v';
-        if (options.uncheckable) {
-          return {
-            stderr: 'uncheckable'
-          };
-        } else {
-          return _cachedExec(options.version ? `${cmd} ${option}` : cmd, noCache);
-        }
-      }
-    };
-    const _check = (cmd, options, result) => new Promise((ok, ko) => {
-      let out = {version: options.version};
-      const actual = _getVersion(cmd, result, options);
-      if (options.version) {
-        if (result.status !== 0 && !options.listedIn) {
-          out.error = `'${cmd}' is not found!!`;
-          out.data = result.stderr.toString();
-        } else {
-          if (!semver.satisfies(actual, options.version)) {
-            out.error = `not satisfied by: '${actual}'`;
-          } else {
-            out.message = ['#green', cmd, '(', actual, ') is installed ...', '#green', 'OK'];
-          }
-        }
-      } else {
-        if (result.status === 127) {
-          out.error = `'${cmd}' is not accesible!!`;
-          out.data = result.stderr.toString();
-        } else if (result.status === -100 || (options.listedIn && actual === false)) {
-          out.version = out.version ? out.version : 'any';
-          out.error = `'${options.key}' is not in list!!`;
-          out.data = result.stderr.toString();
-        } else {
-          out.version = out.version ? out.version : 'any';
-          if (options.uncheckable) {
-            out.message = ['#yellow', 'Uncheckable!'];
-          } else {
-            out.message = ['#green', cmd, 'is installed ...',  '#green', 'OK'];
-          }
-        }
-      }
-      this.logger.info.apply(this.logger, ['#cyan', cmd, '(', out.version, ') is required -> '].concat(out.message ? out.message : [out.error, '...', options.uncheckable ? '#green' : '#red', options.uncheckable ? 'OK (uncheckable)' : 'ERROR!']));
-      if (out.error && !options.uncheckable) {
-        ko(out);
-      } else {
-        ok(out);
-      }
-    });
     const _install = (cmd, option) => {
       this.logger.info('#cyan', cmd, 'is required -> ', '#green', cmd, 'is installing...');
-      const installable = this._installable(cmd, option);
+      const installable = this.systemInstallable(cmd, option);
       const cmds = option.cmdInstaller.split(' ');
       return this.execute(cmds[0], cmds.slice(1).concat([ installable ]));
     };
@@ -118,15 +22,15 @@ module.exports = {
       const promises = [];
       Object.getOwnPropertyNames(tmp).forEach((cmd) => {
         const options = this.params.requirements[cmd];
+        const father = this.params.requirements[options.listedIn];
         promises.push(Promise.resolve()
-          .then(() => _sh(cmd, options))
-          .then((result) => _check(cmd, options, result))
+          .then(() => requirements.sh(cmd, options, father))
+          .then((result) => requirements.check(cmd, options, result, father), (result) => requirements.check(cmd, options, result, father))
           .catch((checked) => {
             if (options.installer && this.params.requirements[options.installer]) {
               options.cmdInstaller = this.params.requirements[options.installer].cmdInstaller;
               return _install(cmd, options)
-                .then(() => _sh(cmd, options, true))
-                .then((_result) => _check(cmd, options, _result));
+                .then(() => requirements.sh(cmd, options, father, true))
             } else if (!this.params.neverStop) {
               throw checked;
             }
@@ -138,7 +42,7 @@ module.exports = {
   },
 
   addons: {
-    _installable(cmd, option){
+    systemInstallable(cmd, option){
       let p = option.uri ? `git+${option.uri}` : (option.pkg ? option.pkg : cmd);
       const versionchar = option.uri ? '#' : '@';
       p = option.version ? `${p}${versionchar}${option.version}` : p;
