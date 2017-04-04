1b718377708afe2aad4413cb9bf7ec6f1cf906b4
jesus.diaz <jesus.diaz.contractor@bbva.com>
2017-04-04 16:52:17 +0200

    feat(): avoid duplicate installations
    - check whether a "requires" package is installed or not
    - use the system-checker addon "systemInstall" to install a "requires" package
    
    [159]

--- plugins/installer/index.js
+++ plugins/installer/index.js
@@ -3,25 +3,59 @@
 const fs = require('fs');
 const path = require('path');
 
+const semver = require('semver');
+
+const requirements = require('../../lib/utils/requirements');
+
 const relaunchFile = '.relaunch';
+
 
 module.exports = {
 
   'core-install': function() {
-    const flowInstaller = this._flow && !this.piscoConfig.isInstalledFlow(this._context, this._flow);
-    const stepInstaller = this.requires && !this.installed && flowInstaller;
-    this.params._skip = this.params._skip || flowInstaller;
+    const getRequirement = (pkg, installable) => {
+      const cmd = pkg;  
+      const options = {
+        installer: 'npm',
+        listedIn: 'npm'
+      };
+      const father = this.params.versions[options.installer];
 
-    const install = () => {
+      if (semver.valid(installable) || semver.validRange(installable)) {
+        options.version = installable;
+      } else {
+        options.uri = installable;
+      }
+      
+      return { cmd, options, father };
+    };
+
+
+    const installRequirement = (requirement) => {
+      const cmd = requirement.cmd;
+      const options = requirement.options;
+      const father = requirement.father;
+
+      return Promise.resolve()
+        .then(() => requirements.sh(cmd, options, father))
+        .then(result => requirements.check(cmd, options, result, father))
+        .catch((checked) => {
+          return this.systemInstall(cmd, father)
+            .then(() => requirements.sh(cmd, options, father, true))
+        });
+    };
+
+
+    const installRequirements = () => {
       const promises = [];
-      Object.getOwnPropertyNames(this.requires).forEach((module) => {
-        let installable = this.requires[module];
-        installable = installable.indexOf('git+') >= 0 ? installable : `${module}@${installable}`;
-        this.logger.info('#green', 'installing', module, '->', installable);
-        promises.push(this.execute('npm', ['install', '-g', installable]));
+      Object.getOwnPropertyNames(this.requires).forEach((pkg) => {
+        const installable = this.requires[pkg];
+        const requirement = getRequirement(pkg, installable);
+        promises.push(installRequirement(requirement));
       });
       return Promise.all(promises);
     };
+
 
     const relaunch = () => new Promise((ok, ko) => {
       try {
@@ -33,9 +67,14 @@ module.exports = {
       }
     });
 
+
+    const flowInstaller = this._flow && !this.piscoConfig.isInstalledFlow(this._context, this._flow);
+    const stepInstaller = this.requires && !this.installed && flowInstaller;
+    this.params._skip = this.params._skip || flowInstaller;
+
     if (stepInstaller) {
       return Promise.resolve()
-        .then(() => install())
+        .then(() => installRequirements())
         .then(() => relaunch())
         .then(() => this.logger.info('#magenta', 'core-install', '#green', 'step installed'))
         .catch((err) => {
