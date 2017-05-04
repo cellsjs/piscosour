b292d19f2f35e8cc0da8ee00b9b33fb4ca7d88ec
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2017-05-04 12:46:05 +0200

    test() Strict mode
    strict mode in unit tests
    
    [QPA-91]

--- test/config-test.js
+++ test/config-test.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 const config = require('../lib/config.js');
 const assert = require('assert');
--- test/index-test.js
+++ test/index-test.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const pisco = require('../index.js');
 const assert = require('assert');
 const fs = require('fs');
