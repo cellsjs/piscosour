77994fbddc4a6edc905323d95e17a0d29e0df8ba
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2017-05-25 16:58:35 +0200

    test() Lint in tests
    Lint in tests
    
    QPA-78

--- test/bus-test.js
+++ test/bus-test.js
@@ -5,26 +5,28 @@ const it = require('mocha').it;
 const bus = require('../lib/bus.js');
 const assert = require('assert');
 
+/* global define, it, describe, before */
 describe.skip('Testing the bus.js file', () => {
-    it('Should emit correctly', () => {
-        //Arrange
-        //Act
-        let busOut = bus.emit();
-        //Assert
-        assert.ok(busOut, 'Getting the bus from emit');
-        assert.ok(busOut.on, 'Getting the on function property from the bus');
-        assert.ok(busOut.emit, 'Getting the emit function property from the bus');
-    })
-    it.skip('Should execute on correctly', () => {
-        //Arrange
-        //Act
-        let busOut = bus.on(assertExitCalled);
-        //Assert
-        assert.ok(busOut, 'Getting the bus from emit');
-        function assertExitCalled() {
-            assert.ok('We have called the callback exit correctly');
-        }
-        assert.ok(busOut.on, 'Getting the on function property from the bus');
-        assert.ok(busOut.emit, 'Getting the emit function property from the bus');
-    })
+  it('Should emit correctly', () => {
+    //Arrange
+    //Act
+    let busOut = bus.emit();
+    //Assert
+    assert.ok(busOut, 'Getting the bus from emit');
+    assert.ok(busOut.on, 'Getting the on function property from the bus');
+    assert.ok(busOut.emit, 'Getting the emit function property from the bus');
+  })
+  it.skip('Should execute on correctly', () => {
+    //Arrange
+    //Act
+    let busOut = bus.on(assertExitCalled);
+    //Assert
+    assert.ok(busOut, 'Getting the bus from emit');
+    function assertExitCalled() {
+      assert.ok('We have called the callback exit correctly');
+    }
+
+    assert.ok(busOut.on, 'Getting the on function property from the bus');
+    assert.ok(busOut.emit, 'Getting the emit function property from the bus');
+  })
 })
